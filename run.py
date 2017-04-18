ae21b12b4746ec21c07c890b60817a589e6c9c7f
Leonardo Santos Monteiro <monteiro.leonardosantos@gmail.com>
2017-04-18 01:53:17 -0300

    improving code

--- run.py
+++ run.py
@@ -1,28 +1,25 @@
+""" This file execute de switch betten workspaces. """
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-from subprocess import check_output, run
+from subprocess import run
+import os
 
-file = open('file', 'r')
+HOME_DIR = os.environ['HOME']
+FILE = open(HOME_DIR + '/.workspaces', 'r')
 
-command = 'xdotool get_desktop'
-# active_workspace = 0
+EXEC = 'xdotool get_desktop'
 
-# if int(check_output(command, shell=True)) == 0:
-#     print('1111111111111111111')
-# else:
-#     print('DIOASNIODNAS')
-
-if int(file.read()) == 0:
-    command = 'xdotool set_desktop 1'
-    file.close()
-    file = open('file', 'w')
-    file.write('1')
+if int(FILE.read()) == 0:
+    EXEC = 'xdotool set_desktop 1'
+    FILE.close()
+    FILE = open(HOME_DIR + '/.workspaces', 'w')
+    FILE.write('1')
 else:
-    command = 'xdotool set_desktop 0'
-    file.close()
-    file = open('file', 'w')
-    file.write('0')
+    EXEC = 'xdotool set_desktop 0'
+    FILE.close()
+    FILE = open(HOME_DIR + '/.workspaces', 'w')
+    FILE.write('0')
 
-run(command, shell=True)
+run(EXEC, shell=True)
 
-file.close()
+FILE.close()
