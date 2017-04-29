d1ac90c64082b9caad58db7459d62409c6e6e85f
quatroka <monteiro.leonardosantos@gmail.com>
2017-04-29 01:50:59 -0300

    update script to execute to infinite! without write and read files

--- run.py
+++ run.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """ This file execute de switch betten workspaces. """
-from subprocess import run
+from subprocess import run, check_output
 import os
 
-ROOT_DIR = os.environ['HOME'] + '/.workspace-switcher'
-FILE = open(ROOT_DIR + '/workspaces', 'r')
+ACTUAL_DESKTOP = int(check_output('xdotool get_desktop', shell=True))
+NUM_DESKTOPS = int(check_output('xdotool get_num_desktops', shell=True))
 
-EXEC = 'xdotool get_desktop'
+if ACTUAL_DESKTOP == NUM_DESKTOPS - 1:
+    ACTUAL_DESKTOP = -1
 
-if int(FILE.read()) == 0:
-    EXEC = 'xdotool set_desktop 1'
-    FILE.close()
-    FILE = open(ROOT_DIR + '/workspaces', 'w')
-    FILE.write('1')
-else:
-    EXEC = 'xdotool set_desktop 0'
-    FILE.close()
-    FILE = open(ROOT_DIR + '/workspaces', 'w')
-    FILE.write('0')
-
-FILE.close()
-
+EXEC = 'xdotool set_desktop {0}'.format(ACTUAL_DESKTOP + 1)
 run(EXEC, shell=True)
+
+print(ACTUAL_DESKTOP)
+print(NUM_DESKTOPS)
+
+# ROOT_DIR = os.environ['HOME'] + '/.workspace-switcher'
+# FILE = open(ROOT_DIR + '/workspaces', 'w')
+# FILE.write(str(ACTUAL_DESKTOP + 1))
+# FILE.close()
+
+# FILE = open(ROOT_DIR + '/workspaces', 'r')
+# FILE.close()
+# # ACTUAL_DESKTOP = int(FILE.read())
+# run('xdotool get_desktop', shell=True)
